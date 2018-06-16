ebdafe1389f2ea0825dee71b1bf7f614fcad61f7
suzannex <becker.su@husky.neu.edu>
2018-06-16 14:19:11 -0400

    set up files

---
+++ coffee-packing.js
@@ -0,0 +1,27 @@
+// Slurp Developer Assignment
+
+/*
+Classes:
+
+Bag
+- dimensions
+- volume (func or prop)
+
+Box
+- dimension
+- list of bags it has? don't need maybe
+..brainstorming how to store fullness info...
+- 2d array of heights of stacked material at each cc
+=>> ok that's not scalable, or at least is not efficient to search over
+=>> but honestly, just start with the slow solution b/c at least it's something
+=>> Invariant: at any point x,y in arr, all points <x or <y have = or greater heights.
+- list of bags/orientations and bottom left corner coords
+*/
+
+
+
+// read in args
+// set values: size of box, # of each size bag
+// program var: array of boxes, init to 1 box w/ right dimensions.
+
+//
