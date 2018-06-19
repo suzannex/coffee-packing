46e3925ba01e978d611b0603e92ea544f34704cb
suzannex <becker.su@husky.neu.edu>
2018-06-18 22:49:58 -0400

    gitignore

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
+Slurp Developer Assignment
+
+
+
+Key simplification is in updating heights in array after adding a bag.
+The following invariant is maintained:
+For any x,y location with value h in the array of bag heights,
+all locations with a smaller x or y have values equal to or greater than h.
+Example:
+If a bag measuring 2x1x3 is added at (1,1) in the following array (where 0,0 is bottom left):
+    0 0 0
+    1 0 0
+    1 2 2
+The array will then look like this:
+    0 0 0
+    3 3 3
+    3 3 3
+
+Programmatically, my definition of 'fit' is that a bag with a certain
+orientation placed at a location would not go past the walls of the box.
+I did not want to have to deal with checking every height value in the area
+that the bag would occupy in order to find out if it would bump into an already
+placed bag.
+- would render location unusable, or would have to just raise the bag to
+the level of the highest bag in its footprint area anyway, which involves my simplification anyway
+
+
+
+
+
+
 Accomplished
 - read inputs
 - put all bags into boxes with no overlap
@@ -8,5 +39,20 @@ Fixed bag orientation (no rotation to find possible fits)
 
 Next Steps (one at a time)
 1. Rotate bags in each spot to find any possible fit
+=>> Rotation mechanism/tracking: 6 possible orientations
+=>> side benefit: cuts down on object creation
 
-2. Choose the best orientation for a bag based on how much space it will waste
+
+
+Things to improve:
+- [done] only need a single instance of each size bag
+- [eh] more generic permutations function
+
+
+
+
+
+
+
+
+.
--- coffee-packing.js
+++ coffee-packing.js
@@ -1,35 +1,17 @@
 // Slurp Developer Assignment
 
-
-// idea from last night:
-// back to the filling rows idea
-// track the next x for a current y to fill
-//  and also track the next y to fill
-// this is  all for a current z
-// then when x and y are no longer fillable
-// change the z
-
-
 /*
-Classes:
-
-Bag
-- dimensions
-- volume (func or prop)
-
-Box
-- dimension
-- list of bags it has? don't need maybe
-..brainstorming how to store fullness info...
-- 2d array of heights of stacked material at each cc
-=>> ok that's not scalable, or at least is not efficient to search over
-=>> but honestly, just start with the slow solution b/c at least it's something
-=>> Invariant: at any point x,y in arr, all points <x or <y have = or greater heights.
-- list of bags/orientations and bottom left corner coords
+This file contains the following sections:
+1. Util functions
+2. Classes
+3. Main functions
 */
 
-// make a 2d array with dim rows and columns, with all values init to 0
-function emptyArray(dim) {
+// ----------------------------------------------------------------------------
+// UTILS
+
+// Make a 2d array with dim rows and columns, with all values init to 0
+function generateZeroHeightArray(dim) {
   var arr = new Array(dim);
   for (let i = 0; i < dim; i++) {
     row = new Array(dim);
@@ -37,11 +19,10 @@ function emptyArray(dim) {
     arr[i] = row;
   }
   return arr;
-  // or:
-  //return new Array(dim).fill(new Array(dim).fill(0));
 }
 
-// print the 2d array nicely
+// ** Used to verify results in testing **
+// Print the 2d array nicely
 function printArray(arr, size) {
   let arrStr = ""
   for (let i = 0; i < size; i++) {
@@ -54,7 +35,15 @@ function printArray(arr, size) {
   console.log(arrStr)
 }
 
+// Create array of all 6 permutations of the 3 elements
+function permutations(a, b, c) {
+  return [[a, b, c], [a, c, b], [b, a, c], [b, c, a], [c, a, b], [c, b, a]];
+}
+
+
+
 // ----------------------------------------------------------------------------
+// CLASSES
 
 class Bag {
   constructor(x, y, z) {
@@ -62,53 +51,64 @@ class Bag {
     this.y = y;
     this.z = z;
   }
-  static newSmallBag() { return new Bag(2, 16, 23); }
-  static newMedBag() { return new Bag(2, 22, 26); }
-  static newLargeBag() { return new Bag(10, 14, 26); }
-
   getX() { return this.x; }
   getY() { return this.y; }
   getZ() { return this.z; }
-
   getVolume() { return this.x * this.y * this.z; }
 
-  // rotate the bag
+  // return array of all possible orientations of a bag with the given dimensions
+  static allBags(x, y, z) {
+    const perms = permutations(x, y, z);
+    let bags = []
+    for (let perm of perms) {
+      bags = bags.concat(new Bag(perm[0], perm[1], perm[2]));
+    }
+    return bags;
+  }
 }
 
-// ----------------------------------------------------------------------------
-
 class Box {
+  /* Instance variables:
+    dimension : the side length of this Box in centimeters
+    bags : a list of all the Bags that have been added to this Box
+    heights : a 2D array of integers representing a top view of the bags
+       in this Box, with each value representing the total height of
+       stacked Bags at that 1cm by 1cm location.   */
   constructor(dimension) {
     this.dimension = dimension;
     this.bags = [];
-    this.heights = emptyArray(dimension);
+    this.heights = generateZeroHeightArray(dimension);
   }
 
-  // add a bag to the heights array and the bag to the bags array,
-  // or return false if the bag can't fit in this box
-  addBag(bag) {
-    // add the bag at the first cell it will fit in
-    for (let row = 0; row < this.dimension; row++) { // row idx is x pos
-      for (let col = 0; col < this.dimension; col++) { // col idx is y pos
-        let cell = this.heights[row][col];
+  // Given an array with the 6 possible x-y-z orientations of a coffee bag,
+  // determine if the bag will fit in this Box. If so, add it to this box.
+  // If not, return false to indicate that the bag does not fit.
+  // @bags -- an array of Bag objects with dimensions in different orientations
+  addBag(bags) {
+    // add the bag at the first x,y,z location it will fit in
+    for (let row = 0; row < this.dimension; row++) { // row idx is x position
+      for (let col = 0; col < this.dimension; col++) { // col idx is y position
+        let height = this.heights[row][col];
+        // try adding the bag at different orientations until it fits
+        for (let bag of bags) {
+          if (this.doesBagFit(bag, row, col, height)) {
+            // prnt this.heights to see if it changed
+            console.log("occ vol before adding bag: " + this.getOccupiedVolume());
+            this.updateHeights(row + bag.getX(), col + bag.getY(), height + bag.getZ())
+            console.log("occ vol after adding bag: " + this.getOccupiedVolume());
 
-        // if the bag is short enough to fit here, add it
-        if (this.doesBagFit(bag, row, col, cell)) {
-          // prnt this.heights to see if it changed
-          console.log("occ vol before adding bag: " + this.getOccupiedVolume());
-          this.updateHeights(row + bag.getX(), col + bag.getY(), cell + bag.getZ())
-          console.log("occ vol after adding bag: " + this.getOccupiedVolume());
-
-          this.bags = this.bags.concat(bag);
-          return true;
+            this.bags = this.bags.concat(bag);
+            return true;
+          }
         }
       }
     }
     return false;
   }
 
-  // If placed at this x,y,z location in the box,
-  // would the bag fit?
+  // Does the bag fit in this box if placed at the given x,y,z location?
+  // @bag -- a Bag
+  // @x, @y, @z -- coordinates in the box (in centimenters)
   doesBagFit(bag, x, y, z) {
     let xFits = (x + bag.getX()) <= this.dimension;
     let yFits = (y + bag.getY()) <= this.dimension;
@@ -117,7 +117,9 @@ class Box {
   }
 
   // From 0,0 to x,y, set all heights in the array to at least
-  // the value of height
+  // as tall as height.
+  // @x, @y -- coordinates in the box (in cm)
+  // @height -- the new height
   updateHeights(x, y, height) {
     for (let i = 0; i < x; i++) {
       for (let j = 0; j < y; j++) {
@@ -128,61 +130,100 @@ class Box {
     }
   }
 
-  // Calculate the volume of this box that is used up by coffee bags
+  // ** This function is just for use in testing. **
+  // Calculate the volume of this box that is recorded as occupied
+  // in the heights array. (sums all numbers in the array)
   getOccupiedVolume() {
-    let sum = 0;
-    for (let i = 0; i < this.dimension; i++) {
-      for (let j = 0; j < this.dimension; j++) {
-        sum += this.heights[i][j];
-      }
-    }
-    return sum;
-    // const rowReducer = (totalVolume, arr) => ( arr.reduce(innerReducer,0) + totalVolume )
-    // const innerReducer = (totalVol, cellVol) => ( cellVol + totalVol )
-    // return this.heights.reduce(rowReducer);
+    /* outerReducer: combine the sums of each inner array in the 2d array */
+    const outerReducer = (totalVolume, row) => ( row.reduce(innerReducer,0) + totalVolume )
+    /* innerReducer: add all the numbers in an array */
+    const innerReducer = (totalVol, cell) => ( cell + totalVol )
+    return this.heights.reduce(outerReducer,0);
   }
 
 
 }
 
 // ----------------------------------------------------------------------------
+// MAIN FUNCTIONS
 
-// given #s of each size bags and size of box,
-// determine # of boxes needed to hold all bags
+// CONSTANTS -- The bags
+// Arrays of bags in all 6 orientations
+const smallBags = Bag.allBags(23, 16, 2);
+const medBags = Bag.allBags(26, 22, 2);
+const largeBags = Bag.allBags(26, 14, 10);
+
+// Use this function to run the program.
+// Validates inputs then runs the algorithm.
 // int, int, int, int => int
+function main(small, med, large, boxDimension) {
+  let errorMsg = validateArgs(small, med, large, boxDimension);
+  if (errorMsg !== false) {
+    return "invalid input: " + errorMsg;
+  } else {
+    return packBoxes(small, med, large, boxDimension).length;
+  }
+}
+
+// given quantities of each size of bag and the size of the box,
+// determine # of boxes needed to hold all bags
+// int, int, int, int => array of Box
 function packBoxes(small, med, large, boxDimension) {
   var boxes = [new Box(boxDimension)]
   // add the large bags
   for (var i = 0; i < large; i++) {
-    boxes = addBagToBoxes(Bag.newLargeBag(), boxes, boxDimension)
+    boxes = addBagToBoxes(largeBags, boxes, boxDimension)
   }
   // add the medium bags
   for (var i = 0; i < med; i++) {
-    boxes = addBagToBoxes(Bag.newMedBag(), boxes, boxDimension)
+    boxes = addBagToBoxes(medBags, boxes, boxDimension)
   }
   // add the small bags
   for (var i = 0; i < small; i++) {
-    boxes = addBagToBoxes(Bag.newSmallBag(), boxes, boxDimension)
+    boxes = addBagToBoxes(smallBags, boxes, boxDimension)
   }
   return boxes;
 }
 
-// Try to fit the bag into some box in the array.
-// if it fits in none, make new box and add it to array.
-// Return the array of boxes containing the bag
-function addBagToBoxes(bag, boxes, boxDimension) {
-  // put the bag into the first box it will fit in.
+// ** Helper function for packBoxes **
+// Try to fit the bag into some box in the box array.
+// If it fits in none, make new box and add it to array.
+// Return the array of boxes that contains the bag
+function addBagToBoxes(bags, boxes, boxDimension) {
   for (let box of boxes) {
-    if (box.addBag(bag)) {
+    if (box.addBag(bags)) {
       return boxes;
     }
   }
-  // if it didn't fit into any of the boxes, add another box for the bag.
+  // add new box for the bag if it didn't fit in any existing box
   var anotherBox = new Box(boxDimension);
-  anotherBox.addBag(bag);
+  anotherBox.addBag(bags);
   return boxes.concat(anotherBox);
 }
 
+// ** Helper function for main function **
+// Validate program arguments + argument types
+function validateArgs(small, med, large, boxDimension) {
+  let errorMsg = false;
+  if (small < 0) {
+    errorMsg = "# of 200g bags is less than 0: " + small;
+  } else if (med < 0) {
+    errorMsg = "# of 400g bags is less than 0: " + med;
+  } else if (large < 0) {
+    errorMsg = "# of 1000g bags is less than 0: " + large;
+  } else if (!(boxDimension >= 30 && boxDimension <= 100)) {
+    errorMsg = "box dimension is not between 30 and 100 cm: " + boxDimension;
+  } else if (!Number.isInteger(small)) {
+    errorMsg = "non-integer argument: " + small;
+  } else if (!Number.isInteger(med)) {
+    errorMsg = "non-integer argument: " + med;
+  } else if (!Number.isInteger(large)) {
+    errorMsg = "non-integer argument: " + large;
+  } else if (!Number.isInteger(boxDimension)) {
+    errorMsg = "non-integer argument: " + boxDimension;
+  }
+  return errorMsg;
+}
 
 
 // read in args
@@ -197,3 +238,5 @@ const actualVolume = boxes[0].bags.reduce((acc, bag) => (acc + bag.getVolume()),
 console.log("actual volume: " + actualVolume);
 console.log(boxes[0].bags);
 printArray(boxes[0].heights, boxes[0].dimension);
+console.log(boxes[1].bags);
+printArray(boxes[1].heights, boxes[1].dimension);
